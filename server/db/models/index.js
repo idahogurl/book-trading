a8d3ff58d3b9069191081d273e22edec612570cd
Rebecca Vest <dynamicdiner@gmail.com>
2018-11-01 14:30:39 -0600

    Update index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -7,20 +7,22 @@ var fs = require('fs');
 var path = require('path');
 var Sequelize = require('sequelize');
 
-var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || 'development';
-var config = require(`${__dirname}/../config/config.json`)[env];
 var db = {};
 
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  var sequelize = require('sequelize-heroku').connect(Sequelize);
-} else {
-  // the application is executed on the local machine ... use sqlite
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+try {
+  var basename = path.basename(__filename);
+  var env = process.env.NODE_ENV || 'development';
+  var config = require(`${__dirname}/../config/config.json`)[env];
 
-fs
+  if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    var sequelize = require('sequelize-heroku').connect(Sequelize);
+  } else {
+    // the application is executed on the local machine ... use sqlite
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
+  
+  fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
@@ -28,13 +30,16 @@ fs
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+  Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+} catch (e) {
+  console.error(e);
+}
 
 module.exports = db;
